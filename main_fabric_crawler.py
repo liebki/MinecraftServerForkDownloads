6cddc838e30521d25341218894ef6e3d6881761c
Kim <kxmliebl@gmail.com>
2025-01-09 13:18:33 +0100

    changed format of fabric jsons to dict

--- main_fabric_crawler.py
+++ main_fabric_crawler.py
@@ -1,54 +1,102 @@
 import requests
 import json
 
-def save_to_json(data, filename: str):
+
+def fetch_data(url: str) -> list:
+    """
+    Fetches JSON data from a given URL.
+
+    Args:
+        url (str): The URL to fetch data from.
+
+    Returns:
+        list: The JSON data as a list.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def filter_versions(game_versions: list, stable: bool) -> list:
+    """
+    Filters game versions based on their stability.
+
+    Args:
+        game_versions (list): List of game version data.
+        stable (bool): True for stable versions, False for non-stable versions.
+
+    Returns:
+        list: List of filtered game version strings.
+    """
+    return [
+        version["version"] for version in game_versions if version["stable"] == stable
+    ]
+
+
+def construct_download_urls(
+    versions: list, loader_version: str, installer_version: str
+) -> dict:
+    """
+    Constructs a dictionary of download URLs for given game versions.
+
+    Args:
+        versions (list): List of game version strings.
+        loader_version (str): The loader version.
+        installer_version (str): The installer version.
+
+    Returns:
+        dict: A dictionary with game versions as keys and download URLs as values.
+    """
+    download_url_template = (
+        "https://meta.fabricmc.net/v2/versions/loader/{}/{}//{}/server/jar"
+    )
+    download_urls = {}
+
+    for version in versions:
+        download_url = download_url_template.format(
+            version, loader_version, installer_version
+        )
+        download_urls[version] = download_url
+
+    return download_urls
+
+
+def save_to_json(data: dict, filename: str):
     """
     Saves given data to a JSON file.
 
     Args:
-        data (dict or list): The data to write to the file.
+        data (dict): The data to write to the file.
         filename (str): The name of the output file.
     """
     with open(filename, "w") as json_file:
         json.dump(data, json_file, indent=4)
     print(f"Download links saved to {filename}")
 
-url_game_versions = "https://meta.fabricmc.net/v2/versions/game"
-game_versions_response = requests.get(url_game_versions)
-game_versions_data = game_versions_response.json()
 
-stable_game_versions = [version['version'] for version in game_versions_data if version['stable']] # release
-non_stable_game_versions = [version['version'] for version in game_versions_data if not version['stable']] # snapshot
+def main():
+    game_versions = fetch_data("https://meta.fabricmc.net/v2/versions/game")
+    loader_versions = fetch_data("https://meta.fabricmc.net/v2/versions/loader")
+    installer_versions = fetch_data("https://meta.fabricmc.net/v2/versions/installer")
 
-url_loader = "https://meta.fabricmc.net/v2/versions/loader"
-loader_response = requests.get(url_loader)
-loader_data = loader_response.json()
-latest_loader_version = loader_data[0]['version']
+    latest_loader_version = loader_versions[0]["version"]
+    latest_installer_version = installer_versions[0]["version"]
 
-url_installer = "https://meta.fabricmc.net/v2/versions/installer"
-installer_response = requests.get(url_installer)
-installer_data = installer_response.json()
-latest_installer_version = installer_data[0]['version']
+    stable_versions = filter_versions(game_versions, stable=True)
+    non_stable_versions = filter_versions(game_versions, stable=False)
 
-stable_download_urls = []
-non_stable_download_urls = []
-download_url_template = "https://meta.fabricmc.net/v2/versions/loader/{}/{}//{}/server/jar"
+    stable_downloads = construct_download_urls(
+        stable_versions, latest_loader_version, latest_installer_version
+    )
 
-for minecraft_version in stable_game_versions:
-    download_url = download_url_template.format(minecraft_version, latest_loader_version, latest_installer_version)
-    stable_download_urls.append({
-        "minecraft_version": minecraft_version,
-        "download_url": download_url
-    })
+    non_stable_downloads = construct_download_urls(
+        non_stable_versions, latest_loader_version, latest_installer_version
+    )
 
-for minecraft_version in non_stable_game_versions:
-    download_url = download_url_template.format(minecraft_version, latest_loader_version, latest_installer_version)
-    non_stable_download_urls.append({
-        "minecraft_version": minecraft_version,
-        "download_url": download_url
-    })
+    save_to_json(stable_downloads, "release_fabric_downloads.json")
+    save_to_json(non_stable_downloads, "snapshot_fabric_downloads.json")
+    print("done")
 
-save_to_json(stable_download_urls, "release_fabric_downloads.json")
-save_to_json(non_stable_download_urls, "snapshot_fabric_downloads.json")
 
-print("done")
+if __name__ == "__main__":
+    main()
